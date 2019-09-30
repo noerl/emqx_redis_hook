95d581f90c73e9c0268a75ae78eb13c550d83ae0
tubin02 <tubin02@meituan.com>
2019-09-30 17:43:35 +0800

    init

--- src/emqx_redis_hook_app.erl
+++ src/emqx_redis_hook_app.erl
@@ -12,9 +12,18 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Opts = [
+        {init_nodes,[
+            {"127.0.0.1",6379}
+        ]},
+        {pool_size, 5},
+        {pool_max_overflow, 0}
+    ],
+    {ok, _RedisPid} = eredis_cluster:start_pool(eredis_pool, Opts),
     {ok, Sup} = emqx_redis_hook_sup:start_link(),
     ?APP:register_metrics(),
     ?APP:load(),
+    emqx_redis_hook_cfg:register(),
     {ok, Sup}.
 
 stop(_State) ->
